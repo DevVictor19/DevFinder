d2825aac5d5301f381c38d29ea22b3b18091e7c0
Antonio Victor <Antoniovictor12@live.com>
2022-12-20 17:29:34 -0300

    feat(App): adicionar inicio do app + thema

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { useTheme } from "./hooks/useTheme";
+
+import moonIcon from "./assets/moon.svg";
+import sunIcon from "./assets/sun.svg";
 
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { theme, setTheme } = useTheme();
+
+  console.log("render");
+
+  function changeTheme() {
+    setTheme((lastTheme) => (lastTheme === "dark" ? "light" : "dark"));
+  }
+
+  return (
+    <main className="bg-light-background dark:bg-dark-background min-h-screen flex justify-center ">
+      <div className="max-w-[730px] w-full mt-[144px]">
+        <div className="flex justify-between text-light-text dark:text-dark-text">
+          <h1 className="text-[26px] font-bold">devFinder</h1>
+          <button
+            className="flex items-center gap-4 font-bold text-[13px] tracking-[2.5px]"
+            onClick={changeTheme}
+          >
+            {theme === "dark" ? "LIGHT" : "DARK"}
+            <img src={theme === "dark" ? sunIcon : moonIcon} alt="moon icon" />
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
