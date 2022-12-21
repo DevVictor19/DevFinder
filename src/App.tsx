b43b945112421b047e12073f2a96c1b3cd60ff77
Antonio Victor <Antoniovictor12@live.com>
2022-12-21 14:31:20 -0300

    feat: utilizar componente com o nome atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { AppMenu } from "./components/AppMenu";
+import { Menu } from "./components/Menu/";
 import { UserSearch } from "./components/UserSearch";
 
 export function App() {
   return (
     <main className="bg-[#F6F8FF] dark:bg-[#141D2F] min-h-screen flex justify-center">
-      <div className="max-w-[730px] w-full mt-[144px]">
-        <AppMenu />
+      <div className="max-w-[730px] w-full mt-[144px] mb-12">
+        <Menu />
         <UserSearch />
       </div>
     </main>
