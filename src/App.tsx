2dfc3c0cb621f0a74c3170e276e5449f48eb5ecf
Antonio Victor <Antoniovictor12@live.com>
2022-12-20 22:20:24 -0300

    feat: adicionar cor texto botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { useTheme } from "./hooks/useTheme";
-
+import { UserSearch } from "./components/UserSearch";
 import moonIcon from "./assets/moon.svg";
 import sunIcon from "./assets/sun.svg";
 
 export function App() {
   const { theme, setTheme } = useTheme();
 
-  console.log("render");
+  console.log("render App");
 
   function changeTheme() {
     setTheme((lastTheme) => (lastTheme === "dark" ? "light" : "dark"));
   }
 
   return (
-    <main className="bg-light-background dark:bg-dark-background min-h-screen flex justify-center ">
+    <main className="bg-light-background dark:bg-dark-background min-h-screen flex justify-center">
       <div className="max-w-[730px] w-full mt-[144px]">
         <div className="flex justify-between text-light-text dark:text-dark-text">
           <h1 className="text-[26px] font-bold">devFinder</h1>
           <button
-            className="flex items-center gap-4 font-bold text-[13px] tracking-[2.5px]"
+            className="flex items-center gap-4 font-bold text-light-soft-600
+            dark:text-dark-soft-600 text-[13px] tracking-[2.5px] group
+            hover:text-light-soft-h-600
+            dark:hover:text-dark-soft-h-600"
             onClick={changeTheme}
           >
             {theme === "dark" ? "LIGHT" : "DARK"}
             <img src={theme === "dark" ? sunIcon : moonIcon} alt="moon icon" />
           </button>
         </div>
+        <UserSearch />
       </div>
     </main>
   );
