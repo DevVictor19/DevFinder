706d6d1d6f351a09666523b0762cce7b83342203
Antonio Victor <Antoniovictor12@live.com>
2022-12-22 22:39:50 -0300

    refactor: deletar divs desnecessarias

--- src/components/UserSearch/UserSocialMedias.tsx
+++ src/components/UserSearch/UserSocialMedias.tsx
@@ -11,26 +11,26 @@ export function UserSocialMedias({ blog, company, location, twitter }: Props) {
   return (
     <ul className="mt-9 grid grid-cols-2 gap-y-5 gap-x-16">
       <li className="flex gap-4">
-        <div
+        <MapPin
+          size={20}
+          weight="fill"
           className={`text-[#4B6A9B] ${
             location ? "dark:text-white" : "text-[#4B6A9B]"
           }`}
-        >
-          <MapPin size={20} weight="fill" />
-        </div>
+        />
         <p className="font-normal text-[15px] text-[#4B6A9B] dark:text-white">
           {location ?? <span className="text-slate-400">Not Avaliable</span>}
         </p>
       </li>
 
       <li className="flex gap-4">
-        <div
+        <TwitterLogo
+          size={20}
+          weight="fill"
           className={`text-[#4B6A9B] ${
             twitter ? "dark:text-white" : "text-[#4B6A9B]"
           }`}
-        >
-          <TwitterLogo size={20} weight="fill" />
-        </div>
+        />
         <p className="font-normal text-[15px] text-[#4B6A9B] dark:text-white">
           {twitter ? (
             `@${twitter}`
@@ -41,13 +41,13 @@ export function UserSocialMedias({ blog, company, location, twitter }: Props) {
       </li>
 
       <li className="flex gap-4">
-        <div
+        <Link
+          size={20}
+          weight="fill"
           className={`text-[#4B6A9B] ${
             blog ? "dark:text-white" : "text-[#4B6A9B]"
           }`}
-        >
-          <Link size={20} weight="fill" />
-        </div>
+        />
         <p className="font-normal text-[15px] text-[#4B6A9B] dark:text-white">
           {blog ? (
             <a className="hover:underline" href={blog} target="_blank">
@@ -60,13 +60,13 @@ export function UserSocialMedias({ blog, company, location, twitter }: Props) {
       </li>
 
       <li className="flex gap-4">
-        <div
+        <Buildings
+          size={20}
+          weight="fill"
           className={`text-[#4B6A9B] ${
             company ? "dark:text-white" : "text-[#4B6A9B]"
           }`}
-        >
-          <Buildings size={20} weight="fill" />
-        </div>
+        />
         <p className="font-normal text-[15px] text-[#4B6A9B] dark:text-white">
           {company ?? <span className="text-slate-400">Not Avaliable</span>}
         </p>
