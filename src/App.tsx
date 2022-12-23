53e63adeacd7e1788fec9f6a52a43e2defcdfe19
Antonio Victor <Antoniovictor12@live.com>
2022-12-23 11:34:46 -0300

    feat: adicionar responsividade tablet

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { UserSearch } from "./components/UserSearch";
 
 export function App() {
   return (
-    <main className="bg-[#F6F8FF] dark:bg-[#141D2F] min-h-screen flex justify-center">
+    <main className="bg-[#F6F8FF] dark:bg-[#141D2F] min-h-screen flex justify-center px-24">
       <div className="max-w-[730px] w-full mt-[144px] mb-12">
         <Menu />
         <UserSearch />
--- src/components/UserSearch/UserResume.tsx
+++ src/components/UserSearch/UserResume.tsx
@@ -15,32 +15,40 @@ export function UserResume({ user }: Props) {
   return (
     <article
       className="mt-6 bg-[#FEFEFE] dark:bg-[#1E2A47] rounded-[15px]
-        shadow-custom dark:shadow-none p-12 flex gap-9 min-h-[444px]"
+        shadow-custom dark:shadow-none p-12 min-h-[444px]"
     >
-      <div className="w-[117px] h-[117px] rounded-full overflow-hidden shrink-0">
-        <img src={user.avatar_url} alt="avatar img" />
-      </div>
-      <section className="w-full">
-        <div className="flex justify-between items-center">
-          <h1
-            className="font-bold text-[26px] text-[#2B3442] 
+      <section className="flex gap-9 justify-center">
+        <div className="w-[117px] h-[117px] rounded-full overflow-hidden shrink-0">
+          <img src={user.avatar_url} alt="avatar img" />
+        </div>
+        <div
+          className="flex max-[890px]:flex-col justify-between max-[890px]:justify-start 
+          items-baseline w-full"
+        >
+          <div>
+            <h1
+              className="font-bold text-[26px] text-[#2B3442] 
           dark:text-white"
-          >
-            {user.name ?? "No name provided"}
-          </h1>
-          <h2
+            >
+              {user.name ?? "No name provided"}
+            </h1>
+            <h2 className="mt-0.5 font-normal text-base text-[#0079FF]">
+              @{user.login}
+            </h2>
+          </div>
+          <h3
             className="font-normal text-[15px] text-[#697C9A] 
-          dark:text-slate-300"
+          dark:text-slate-300 max-[890px]:mt-1"
           >
             {`Joined ${day} ${month} ${year}`}
-          </h2>
+          </h3>
         </div>
-        <h3 className="mt-0.5 font-normal text-base text-[#0079FF]">
-          @{user.login}
-        </h3>
+      </section>
+
+      <section className="max-w-[480px] w-full max-[890px]:max-w-none ml-[153px] max-[890px]:ml-0">
         <p
-          className="mt-5 font-normal text-[15px] text-[#4B6A9B] 
-          dark:text-white"
+          className="font-normal text-[15px] text-[#4B6A9B] 
+          dark:text-white mt-[-30px] max-[890px]:mt-6"
         >
           {user.bio ?? (
             <span className="text-slate-500">This profile has no bio</span>
